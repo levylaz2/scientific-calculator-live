aaf0eeb9ff56480b33514529c3841e66e2bf4d61
levylaz2 <levilazarus68@gmail.com>
2025-02-07 00:06:17 +0000

    implemented a few scientific functions

--- src/script.js
+++ src/script.js
@@ -10,7 +10,17 @@ document.addEventListener("DOMContentLoaded", function () {
         const convertedValue = currentValue
             .replace("×", "*")
             .replace("÷", "/")
-            .replace('%', '*0.01');
+            .replace('%', '*0.01')
+            .replace('sin', 'Math.sin')
+            .replace('cos', 'Math.cos')
+            .replace('ln', 'Math.log')
+            .replace( 'π', 'Math.PI')
+            .replace( 'log', 'Math.log10')
+            .replace( 'e', 'Math.E')
+            .replace( 'tan', 'Math.tan')
+            .replace('√' , 'Math.sqrt');
+
+            
 
         console.log('convertedValue:', convertedValue)
         const result = eval(convertedValue);
@@ -25,15 +35,23 @@ document.addEventListener("DOMContentLoaded", function () {
         button.addEventListener('click', function () {
             const value = button.innerText;
 
-            if (value == "AC") {
-                currentValue = "";
-                display.value = currentValue;
-            } else if (value == "=") {
-                evaluateResult();
-            } else {
-                currentValue += value;
+            try{
+                if (value == "AC") {
+                    currentValue = "";
+                    display.value = currentValue;
+                } else if (value == "=") {
+                    evaluateResult();
+                } else {
+                    currentValue += value;
+                    display.value = currentValue;
+                }
+            } catch (error) {
+                console.error(error);
+                currentValue = "ERROR";
                 display.value = currentValue;
             }
+
+            
         })
 
     }
